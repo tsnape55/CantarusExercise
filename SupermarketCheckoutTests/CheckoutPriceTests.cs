a60842b3573369dc01e7b7735dfe3092e91441f9
Tom <tomsnape55@gmail.com>
2020-10-07 14:11:39 +0100

    Added in depth checkout price tests

--- SupermarketCheckoutTests/CheckoutPriceTests.cs
+++ SupermarketCheckoutTests/CheckoutPriceTests.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SupermarketCheckout;
 using SupermarketInterfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SupermarketCheckoutTests
 {
@@ -14,34 +9,133 @@ namespace SupermarketCheckoutTests
     public class CheckoutPriceTests
     {
         [TestMethod]
-        public void Accurate_Price()
+        public void Subtotal_Returns_Expected_Price()
         {
+            // Arrange.
             IPricings pricing = new Pricings();
             pricing.AddProduct("A", 5f);
             pricing.AddProduct("B", 3.99f);
             ICheckout checkout = new Checkout();
             checkout.Configure(pricing);
+
+            // Act
+            checkout.Scan("A");
             checkout.Scan("A");
             checkout.Scan("A");
             checkout.Scan("B");
 
-            Assert.AreEqual(13.99f, checkout.Subtotal());
+            //Assert
+            Assert.AreEqual(18.99f, checkout.Subtotal());
         }
 
         [TestMethod]
-        public void Accurate_Price_WithDeal()
+        public void All_Value_Functions_Return_0_When_No_Products_Scan()
         {
+
+            // Arrange.
+            IPricings pricing = new Pricings();
+            pricing.AddProduct("A", 5f);
+            ICheckout checkout = new Checkout();
+            checkout.Configure(pricing);
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(0f, checkout.Savings());
+            Assert.AreEqual(0f, checkout.Total());
+            Assert.AreEqual(0f, checkout.Subtotal());
+        }
+
+        [TestMethod]
+        public void Savings_Returns_0_When_No_Deals_Added()
+        {
+
+            // Arrange.
+            IPricings pricing = new Pricings();
+            pricing.AddProduct("A", 5f);
+            pricing.AddProduct("B", 3.99f);
+            ICheckout checkout = new Checkout();
+            checkout.Configure(pricing);
+
+            // Act
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            //Assert
+            Assert.AreEqual(0f, checkout.Savings());
+        }
+
+        [TestMethod]
+        public void Savings_Returns_Expected_Price()
+        {
+            // Arrange.
             IPricings pricing = new Pricings();
             pricing.AddProduct("A", 5f);
             pricing.AddProduct("B", 3.99f);
             pricing.AddDeal("A", 2, 8f);
             ICheckout checkout = new Checkout();
             checkout.Configure(pricing);
+
+            // Act
             checkout.Scan("A");
             checkout.Scan("A");
             checkout.Scan("B");
 
-            Assert.AreEqual(11.99f, checkout.Total());
+            //Assert
+            Assert.AreEqual(2f, checkout.Savings());
+        }
+
+        [TestMethod]
+        public void Total_Returns_Expected_Price_Multiple_Deals_Chooses_Highest_Saving()
+        {
+
+            // Arrange.
+            IPricings pricing = new Pricings();
+            pricing.AddProduct("A", 5f);
+            pricing.AddProduct("B", 3.99f);
+            pricing.AddDeal("A", 2, 8f);
+            pricing.AddDeal("A", 3, 12f);
+            ICheckout checkout = new Checkout();
+            checkout.Configure(pricing);
+
+            // Act
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            //Assert
+            Assert.AreEqual(15.99f, checkout.Total());
+
+            //Assert
+            Assert.AreEqual(3f, checkout.Savings());
+            Assert.AreEqual(18.99f, checkout.Subtotal());
+            Assert.AreEqual(15.99f, checkout.Total());
+        }
+
+        [TestMethod]
+        public void Savings_Returns_Expected_Price_Multiple_Deals()
+        {
+            // Arrange.
+            IPricings pricing = new Pricings();
+            pricing.AddProduct("A", 1f);
+            pricing.AddProduct("B", 2f);
+            pricing.AddDeal("A", 2, 1.50f);
+            pricing.AddDeal("B", 2, 3f);
+            ICheckout checkout = new Checkout();
+            checkout.Configure(pricing);
+
+            // Act
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("B");
+
+            //Assert
+            Assert.AreEqual(1.5F, checkout.Savings());
+            Assert.AreEqual(6F, checkout.Subtotal());
+            Assert.AreEqual(4.5F, checkout.Total());
         }
 
     }
